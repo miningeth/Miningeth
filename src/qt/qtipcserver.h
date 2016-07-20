99d01f50be5005ff60aaf1bc9fc062842386b906
MiningETH <miningeth@yandex.ru>
2016-07-21 01:20:07 +0500

    new updated

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define MiningEth-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "MiningEthURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
